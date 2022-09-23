298e8cfacb27cee238a5ce661e9661d6593e63d9
berrezougAbdelhak <a.berrezoug@esi-sba.dz>
2022-09-23 17:35:26 +0100

    Create and get store

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask,jsonify
+import json
+from flask import Flask,jsonify,request
 
 app = Flask(__name__)
 
@@ -9,13 +10,22 @@ stores = [{"name": "My Wonderful Store", "items": [
 
 @app.route("/store", methods=["POST"])
 def create_store():
-    pass
+    request_data=request.get_json()
+    new_store={"name":request_data["name"],"items":[]}
+    stores.append(new_store)
+    return jsonify(new_store)
+
 
 # GET /store/<string: name>
 
 @app.route("/store/<string:name>")
 def get_store(name):
-    pass
+    #Iterate over stores
+    for s in stores:
+        if s["name"]==name:
+            return jsonify(s["name"])
+    return  jsonify({"Message":"Not exist"})
+
 
 
 #GET /store
@@ -35,7 +45,9 @@ def create_item_in_store():
 
 @app.route("/store/<string:name>/item")
 def get_item_in_store(name):
-    pass
-
+    for store in stores:
+        if store["name"]==name:
+            return jsonify(store["items"])
+    return jsonify({"message":" Store does not exist"})
 
 app.run(port=5000)
