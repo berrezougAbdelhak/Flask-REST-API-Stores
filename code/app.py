715f752dda01f4e0110024b141e0e4e3e64ee117
berrezougAbdelhak <a.berrezoug@esi-sba.dz>
2022-09-25 04:18:26 +0100

    ItemList resource

--- code/app.py
+++ code/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,request
 from  flask_restful import Resource, Api 
 
 app=Flask(__name__)
@@ -7,17 +7,25 @@ api=Api(app)
 items=[] 
 class Item(Resource):
     def get(self,name):
-        for item in items:
-            if item["name"]==name:
-                return item
-        return {"Message":"item doesn't exist"},404
+        # for item in items:
+        #     if item["name"]==name:
+        #         return item
+        item=filter(lambda item:item["name"]==name,items)
+        # return {"Message":"item doesn't exist"},404
+        return item
     
     def post(self,name):
-        item={"name":name,"price":15.99}
+        data=request.get_json()
+        item={"name":name,"price":data["price"]}
         items.append(item)
 
         return item,201
-
+class ItemList(Resource):
+    def get(self):
+        return {"items":items}
+         
 api.add_resource(Item,"/item/<string:name>") 
+
+api.add_resource(ItemList,"/items")
 
 app.run()
