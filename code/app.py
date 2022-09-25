f91242f62664dc4710af40ab3d90d87fa1ccbf30
berrezougAbdelhak <a.berrezoug@esi-sba.dz>
2022-09-25 05:07:15 +0100

    Use filter to iterate

--- code/app.py
+++ code/app.py
@@ -7,18 +7,17 @@ api=Api(app)
 items=[] 
 class Item(Resource):
     def get(self,name):
-        # for item in items:
-        #     if item["name"]==name:
-        #         return item
-        item=filter(lambda item:item["name"]==name,items)
-        # return {"Message":"item doesn't exist"},404
-        return item
+        #next give us the first item of the list (that returns only one item)
+        #If does't exist next returns None
+        item=next(filter(lambda x:x["name"]==name,items),None) 
+        return {"item":item},200 if item  else 404
     
     def post(self,name):
+        if next(filter(lambda x:x["name"]==name,items),None) is not None:
+            return {"message":"An item with name {} already exists.".format(name)},400
         data=request.get_json()
-        item={"name":name,"price":data["price"]}
+        item={"name":name,"price":data}
         items.append(item)
-
         return item,201
 class ItemList(Resource):
     def get(self):
