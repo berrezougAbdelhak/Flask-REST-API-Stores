af522645dadd4a4bc53912d1dc2aff5b3d2e2e81
berrezougAbdelhak <a.berrezoug@esi-sba.dz>
2022-09-26 03:37:03 +0100

    Control args with reqparser

--- code/app.py
+++ code/app.py
@@ -1,5 +1,6 @@
+from typing_extensions import Required
 from flask import Flask,request
-from  flask_restful import Resource, Api 
+from  flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT,jwt_required
 from security import authenticate,identity
 
@@ -32,6 +33,20 @@ class Item(Resource):
         global items
         items=list(filter(lambda x:x["name"]!=name,items))
         return {"Message": "Item deleted"}
+    
+    def put(self,name):
+        parser=reqparse.RequestParser() #initialise a new object which we can use to parse the request 
+        parser.add_argument("price",type=float,required=True,help="This field cannot be left blank ")
+
+        data=parser.parse_args()
+        print(data["another"])
+        item=next(filter(lambda x:x["name"]==name,items),None)
+        if item is None:
+            item={"name":name,"price":data["price"]}
+            items.append(item)
+        else:
+            item.update(data)
+        return item 
  
 class ItemList(Resource):
     def get(self):
