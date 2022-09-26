84e4552249e8130c24aaed17412b063a7a9ff0c3
berrezougAbdelhak <a.berrezoug@esi-sba.dz>
2022-09-26 01:54:29 +0100

    Add Json Web Token (JWT)

--- code/app.py
+++ code/app.py
@@ -1,11 +1,19 @@
 from flask import Flask,request
 from  flask_restful import Resource, Api 
+from flask_jwt import JWT,jwt_required
+from security import authenticate,identity
+
 
 app=Flask(__name__)
-
+app.secret_key="jose"
 api=Api(app)
+
+jwt=JWT(app,authenticate,identity)
+
+
 items=[] 
 class Item(Resource):
+    @jwt_required()
     def get(self,name):
         #next give us the first item of the list (that returns only one item)
         #If does't exist next returns None
@@ -16,7 +24,7 @@ class Item(Resource):
         if next(filter(lambda x:x["name"]==name,items),None) is not None:
             return {"message":"An item with name {} already exists.".format(name)},400
         data=request.get_json()
-        item={"name":name,"price":data}
+        item={"name":name,"price":data["price"]}
         items.append(item)
         return item,201
 class ItemList(Resource):
--- code/security.py
+++ code/security.py
@@ -1,4 +1,4 @@
-from werkzeug.security import safe_str_cmp 
+from werkzeug.security import safe_str_cmp
 from user import User
 users=[User(1,"bob","azer")]
 
@@ -9,12 +9,11 @@ userid_mapping={u.id:u for u in users}
 
 def authenticate(username,password):
     user=username_mapping.get(username,None)
-    if user and user.password==password:
+    if user and safe_str_cmp(user.password,password) :
         return user
 
 def identity(payload):
     user_id=payload["identity"]
     return userid_mapping.get(user_id,None)
-
 
 
